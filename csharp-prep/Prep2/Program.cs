0322755470ebb3da1d2c2b6ff075c33239285950
william-johnstone <pailryder@gmail.com>
2023-09-13 18:06:23 -0500

    completed prep2 grade measurer

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.ComponentModel;
 //This is Prep2
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade %? ");
+        string grade = Console.ReadLine();
+        int gradePercent = int.Parse(grade);
+        string letterGrade = "";
+        string signGrade = "";
+        int modGrade;
+
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 80) 
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 70) 
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercent >= 60) 
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        modGrade = gradePercent % 10;
+        //Console.WriteLine($"Mod = {gradeMod}");
+        if (modGrade >= 7) 
+        {
+            signGrade = letterGrade + "+";
+        }
+        else if (modGrade < 3)
+        {
+            signGrade = letterGrade + "-";
+        }
+        else 
+        {
+            signGrade = letterGrade;
+        }
+        if (gradePercent >= 97)
+        {
+            signGrade = letterGrade;
+        }
+        else if (gradePercent < 60)
+        {
+            signGrade = letterGrade;
+        }
+
+        Console.WriteLine($"Your Grade is: {signGrade}");
     }
 }
