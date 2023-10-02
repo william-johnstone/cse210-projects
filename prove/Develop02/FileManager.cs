f722c51448c3b8e7a188987bbb416887bb7a41e6
william-johnstone <pailryder@gmail.com>
2023-10-02 16:03:16 -0500

    writing to text file structure

--- prove/Develop02/FileManager.cs
+++ prove/Develop02/FileManager.cs
@@ -1,21 +1,31 @@
 public class FileManager
 {
     public string _filename = "journal.txt";
-    public List<Entry> entries = new List<Entry>();
+    //public List<Entry> entries = new List<Entry>();
 
     public void FileSave()
     {
+        
+        Console.WriteLine("Saving to file...");
         using (StreamWriter outputFile = new StreamWriter(_filename))
         {
+
             foreach (Entry entry in entries)
             {
-                outputFile.WriteLine(entry);
+                outputFile.WriteLine($"{entry}|");
             }
         }
     }
 
     public void FileLoad()
     {
-        //a;lkjadsf;l
+        Console.WriteLine("Loading from file...");
+        string[] lines = System.IO.File.ReadAllLines(_filename);
+
+        //this needs to get the lines in and get them back in the journal
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
     }
 }
