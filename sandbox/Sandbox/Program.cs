09a73cc3e45e84097ed40aa71a07ea021d0a54db
william-johnstone <pailryder@gmail.com>
2023-10-17 09:34:11 -0500

    using sandbox to test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,34 @@
 using System;
-/*commenting here*/
-
 class Program
 {
+    
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Clear();
+        string userInput = "";
+        //set up the scripture class that need to persist throughout the looping
+        Scripture scripture = new Scripture();
+        
+        //calls the method for ScriptureLoad
+        scripture.ScriptureLoad();
+        //write out the scripture from the OriginalScriptureFormat method
+        Console.WriteLine(scripture.OriginalScriptureFormat);
+        //loop through the scripture or allow quit
+        while (userInput != "quit")
+        {
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            userInput = Console.ReadLine().ToLower();
+            //clears the screen
+            Console.Clear(); 
+            //Write out the scripture from the ModifiedScripture method
+            Console.WriteLine(scripture.ModifiedScripture);
+            if (scripture.Complete)
+            {
+                userInput = "quit";
+            }
+            //check if there are no words left to quit
+        }
+        Console.WriteLine("Goodbye");
     }
 }
